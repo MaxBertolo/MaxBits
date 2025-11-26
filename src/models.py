f04526fcebe1dbcbe7f3a5bae95232304613ce55
Massimo Bertolotti <105781414+MaxBertolo@users.noreply.github.com>
2025-11-26 10:24:26 +0100

    Create models.py

---
+++ src/models.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class RawArticle:
+    id: str
+    title: str
+    url: str
+    source: str
+    published_at: datetime
+    content: str
+    language: str = "en"
